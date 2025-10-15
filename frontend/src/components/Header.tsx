cf825f36fc9cc2852b3ef3af53b58a252ca059e3
gyungmin <gyungmin@gyungminui-MacBookPro.local>
2025-10-15 19:00:22 +0900

    fix: index.html 수정

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -142,7 +142,7 @@ const Header: React.FC<Props> = ({ cartCount, cartTotal, toggleOrder }) => {
               </div>
               <div className="truncate">
                 <h2 className="text-lg font-extrabold tracking-widest drop-shadow text-sky-200">
-                  외대제
+                  New-Path-tival
                 </h2>
                 <p className="text-[11px] text-sky-200/80 font-semibold truncate">
                   제42대 외국어대학 학생회 hear
